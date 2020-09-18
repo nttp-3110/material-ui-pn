c6e774fe815ade1aac8524ef030f591b285b84df
nttp-3110 <nguyenthanhphuong272@gmail.com>
2020-09-18 15:42:59 +0700

    build(input): update behavior

--- src/stories/Form/index.js
+++ src/stories/Form/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import { PnNumberInput as PnTextField } from '../../components/TextField';
+import { PnNumberInput, PnTextInput } from '../../components/TextField';
 import './page.css';
 
 export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
@@ -11,32 +11,32 @@ export const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
       <p>
         <Grid container spacing={3}>
           <Grid item xs={4}>
-            <PnTextField label='Single Save' autoSave />
-            <PnTextField label='label 3' />
+            <PnTextInput label='Single Save' autoSave />
+            <PnTextInput label='label 3' />
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='label 3' required/>
+            <PnTextInput label='label 3' required/>
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='label 3' />
+            <PnTextInput label='label 3' />
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='label 3' />
+            <PnNumberInput label='Number auto save' autoSave />
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='Single Save' autoSave />
+            <PnNumberInput label='Number' />
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='label 3' />
+            <PnTextInput label='label 3' />
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='Label - 4 Col - Idle' autoSave/>
+            <PnTextInput label='Label - 4 Col - Idle' autoSave/>
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='Label - 4 Col' autoSave error errorMessage='Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. '/>
+            <PnTextInput label='Label - 4 Col' autoSave error errorMessage='Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. '/>
           </Grid>
           <Grid item xs={4}>
-            <PnTextField label='Label - 4 Col' error errorMessage='Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. ' />
+            <PnTextInput label='Label - 4 Col' error errorMessage='Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. ' />
           </Grid>
         </Grid>
       </p>
